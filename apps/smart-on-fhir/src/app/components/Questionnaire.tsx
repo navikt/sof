d875cb0f9a871f6030e452b310a74b9e5adec1b3
Royce <56227231+roycelu@users.noreply.github.com>
2021-07-16 10:11:07 +0200

    Display input types (#49)
    * dif helptext exists, display OK
    
    * hjelpetekst OK, men bare nivå 2 #27
    
    * viser tekst fra alle items fra Questionnaire
    
    * alle questionnaire-items rendrer på siden
    
    * la til kommentarerer og ryddet i kode
    
    * lagt til håndtering av hjelpetekst, underspørsmål, hovedspørsmål og riktig input basert på type

--- apps/smart-on-fhir/src/app/components/Questionnaire.tsx
+++ apps/smart-on-fhir/src/app/components/Questionnaire.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ItemAnswer } from './ItemAnswer';
 import questionnaireResponse from '../json-files/questionnaireResponse.json';
 import questionnairePleiepenger from '../json-files/questionnairePleiepenger.json';
@@ -13,80 +13,83 @@ import './questionnaireStylesheet.css';
  */
 export const Questionnaire = () => {
   const questionnaire = questionnairePleiepenger;
-
+  const [questions, setQuestions] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
   const { patient, user } = useFhirContext();
-
-  // TODO: flytte variablene til et annet komponent (QuestionnaireResponse)?
   const [answers, setAnswers] = useState<Map<string, string | boolean>>(
     new Map()
   );
   const response = questionnaireResponse;
 
-  // TODO: Rekursiv metode som itererer seg gjennom item-barna. FUNKER IKKE! :((
-  const checkItemChildren = (result: any) => {
-    if (!('item' in result)) {
-      console.log('if', result.text, result.type);
-      return;
+  // Utkast til logikken for å hente hjelpetekst
+  const getHelptext = (item: any) => {
+    if (item.type === 'display') {
+      if (item.linkId.includes('help')) {
+        return <h1>{item.text}</h1>;
+      }
     }
-    console.log('CIC: ', result.item);
-    return checkItemChildren(result.item);
+    return <></>;
   };
 
-  //console.log('answers: ', answers);
+  // Get the items from the Questionnaire
+  const getItemChildren = (q: any) => {
+    q.item?.map((itemChild: any) => {
+      console.log('C: ', itemChild);
+      if (loading) {
+        setQuestions((prevState) => [...prevState, itemChild]);
+      }
+      if (itemChild && typeof itemChild === 'object') {
+        getItemChildren(itemChild);
+      }
+    });
+  };
 
-  //checkItemChildren(questionnaire);
+  // Laster spørsmålene fra Questionnaire til questions-listen
+  useEffect(() => {
+    getItemChildren(questionnaire);
+    setLoading(false);
+  }, [loading]);
 
   return (
     <>
-      {questionnaire.item.map((value) =>
-        value.linkId.includes('automatic') ? null : (
-          <div key={value.linkId}>
-          {' '}
-          {/*Hovedspørsmål*/}
-          <p className="typo-undertittel">{value.text}</p>
-          <p className="typo-element">{value.text}</p>
-          {/* TODO: Trekke ut <ItemAnswer/> til flere komponenter basert på ønsket inputtype */}
-          {value.item ? (
-            <>
-              {console.log('Children: ', value.item)}
-              {value.item.map((data: any) => (
-                <>
-                  <p className="typo-normal">{}</p>{' '}
-                  {/*Beskrivende/hjelpetekst/undergrupperte spm*/}
-                  <p className="typo-normal">{data.text}</p>{' '}
-                  {/*Beskrivende/hjelpetekst*/}
-                  <ItemAnswer
-                    question={data.text}
-                    linkId={data.linkId}
-                    answerType={data.type}
-                    answers={answers}
-                    setAnswers={setAnswers}
-                  />
-                </>
-              ))}
-            </>
-          ) : null}
-          {/*Svartyper*/}
-          <ItemAnswer
-            question={value.text}
-            linkId={value.linkId}
-            answerType={value.type}
-            answers={answers}
-            setAnswers={setAnswers}
-          />
-        </div>
-        )
-      )}
-      {
-        // TODO: Flytte metoden til et annet komponent (QuestionnaireResponse)?
-        <Hovedknapp
+      {questions.map((item: any) => {
+        return item.linkId.includes('automatic') ? null : (
+          <div key={item.linkId}>
+            {item.linkId.includes('help') ? (
+              <p>
+                {/* Foreløpig håndtering av hjelpetekst*/}
+                {item.linkId} {item.text}
+              </p>
+            ) : item.linkId.includes('.') ? (
+              <p>
+                {/*Foreløpig håndtering av underspørsmål*/}
+                {item.linkId} {item.text}
+              </p>
+            ) : (
+              <h1 className="typo-undertittel">
+                {/* Foreløpig håndtering av hovedspørsmål*/}
+                {item.linkId} {item.text}
+              </h1>
+            )}
+            {/* Svartyper */}
+            <ItemAnswer
+              question={item.text}
+              linkId={item.linkId}
+              answerType={item.type}
+              answers={answers}
+              setAnswers={setAnswers}
+            />
+          </div>
+        );
+      })}
+      <Hovedknapp
           button-general
           onClick={() => saveAnswers(answers, response, patient, user)}
         >
           Lagre
         </Hovedknapp>
-      }
-      {/*console.log('A:', answers)*/}
+
+      {console.log('A:', answers)}
     </>
   );
 };
