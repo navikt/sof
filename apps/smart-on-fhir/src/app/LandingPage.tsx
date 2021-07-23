3cbb6a49a7e70efe61007a0e7d16ee2b60a70486
annvp <86649454+annvp@users.noreply.github.com>
2021-07-23 13:02:37 +0200

    changes on leggtil-button (#108)

--- apps/smart-on-fhir/src/app/LandingPage.tsx
+++ apps/smart-on-fhir/src/app/LandingPage.tsx
@@ -7,13 +7,8 @@ export const LandingPage = () => {
   return (
     <div className="app-container">
       <BannerHeader />
-      <Sidetittel>(Midlertidig) oversikt over utvalgte skjemaer</Sidetittel>
-      <ul>
-        <li>
-          <Link to="/skjema">Legeerklæring: pleiepengeskjema</Link>
-          {/* Husk at "to=.." må matche "path=.." i main.tsx*/}
-        </li>
-      </ul>
+      <Sidetittel>Skjemaer</Sidetittel>
+      <div className="contentContainer"></div>
     </div>
   );
 };
