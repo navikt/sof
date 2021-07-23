6315580c53c6332c1ad781a4dbf970ba29140a4b
Karolill <56150049+Karolill@users.noreply.github.com>
2021-07-23 13:01:49 +0200

    Remove packages 2 (#107)
    * save between
    
    * added dummy function to get practitioners hpr number
    
    * Save to server (#40)
    
    * merged
    
    * merging
    
    * removed api and smart-on-fhir-e2e
    
    Co-authored-by: Royce Lu <roycel@stud.ntnu.no>

--- apps/api/src/routers/constants.ts
+++
@@ -1,7 +0,0 @@
-//Settings for the fhir-server
-export const smartSettings = {
-  clientId: 'my-client-id',
-  redirectUri: '/api/app',
-  scope: 'launch/patient patient/*.read openid fhirUser',
-  iss: 'http://localhost:8080/fhir',
-};
